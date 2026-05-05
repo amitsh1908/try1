154d5c6ddb179a772eed51c33bfd7c6ddfb4e29a
Amit Sharma <amit.sharma@sea-pos.com>
2026-05-05 06:56:30 +0530

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Tell Vite to use the port Helora provides, or fallback to 5173
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    // Required to allow access from outside the container/process
+    host: true, 
+  },
 })
